80db75fa6aebf1f93a1a751042b5650d0f9d497e
Ethan Daniel <ethandaniel@berkeley.edu>
2020-06-29 18:32:51 -0700

    Initial Commit

---
+++ HFT Recurrent Neural Net Status/AppDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  AppDelegate.swift
+//  HFT Recurrent Neural Net Status
+//
+//  Created by Ethan Daniel on 6/29/20.
+//  Copyright © 2020 Ethan Daniel. All rights reserved.
+//
+
+import Cocoa
+import SwiftUI
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Create the SwiftUI view that provides the window contents.
+        let contentView = ContentView()
+
+        // Create the window and set the content view. 
+        window = NSWindow(
+            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
+            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
+            backing: .buffered, defer: false)
+        window.center()
+        window.setFrameAutosaveName("Main Window")
+        window.contentView = NSHostingView(rootView: contentView)
+        window.makeKeyAndOrderFront(nil)
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HFT Recurrent Neural Net Status/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  HFT Recurrent Neural Net Status
+//
+//  Created by Ethan Daniel on 6/29/20.
+//  Copyright © 2020 Ethan Daniel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
