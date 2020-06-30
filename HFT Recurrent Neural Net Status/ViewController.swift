56bb4728fb2a5ceaeea734ccb6853a5b30b22290
Ethan Daniel <ethandaniel@berkeley.edu>
2020-06-29 20:25:11 -0700

    Create prototype layout

---
+++ HFT Recurrent Neural Net Status/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  HFT Recurrent Neural Net Status
+//
+//  Created by Ethan Daniel on 6/29/20.
+//  Copyright © 2020 Ethan Daniel. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+class ViewController : NSViewController {
+    override func viewDidLoad() {
+        
+    }
+    
+    override var representedObject: Any? {
+        didSet {
+            
+        }
+    }
+}
